ec9e21e8dbf97f189231c1022255350b1e91a12e
sandra <mjfelici@gmail.com>
2022-01-11 15:56:25 -0300

    add javascript

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
-const conteudoTips = document.querySelectorAll(".conteudo__btn");
+const conteudoTips = document.querySelectorAll(".conteudo__item");
+const custom = document.getElementById("custom");
 
 conteudoTips.forEach(tip => {
 
     tip.addEventListener("click", (ev) => {
-        const valorConta = document.getElementById("bill");
         const gorgeta = Number(ev.target.value);
+        const valorConta = document.getElementById("bill");
         const numberDePessoas = document.getElementById('nop');
-        console.log({ gorgeta })
 
         const {
             gorgetaPagaPorPessoa,
             valorTotalApagar
         } = calcularDesconto(+valorConta.value, gorgeta, +numberDePessoas.value)
-        exibirConta(gorgetaPagaPorPessoa, valorTotalApagar, numberDePessoas.value)
-        console.log({
-            gorgetaPagaPorPessoa,
-            valorTotalApagar,
 
-        })
+        exibirConta(gorgetaPagaPorPessoa, valorTotalApagar, +numberDePessoas.value)
+
+
     });
+});
+
+
+custom.addEventListener("change", (ev) => {
+    const gorgeta = Number(ev.target.value);
+    const valorConta = document.getElementById("bill");
+    const numberDePessoas = document.getElementById('nop');
+
+    const {
+        gorgetaPagaPorPessoa,
+        valorTotalApagar
+    } = calcularDesconto(+valorConta.value, gorgeta, +numberDePessoas.value)
+    exibirConta(gorgetaPagaPorPessoa, valorTotalApagar, +numberDePessoas.value)
+
 })
+
+
+function calcularCustom(valorConta, gorgeta, numberDePessoas) {
+
+    if (numberDePessoas === 0) {
+        return { gorgetaPagaPorPessoa: 0, valorTotalApagar: 0 };
+    }
+    const valorDaGorgeta = valorConta * gorgeta / 100;
+    const gorgetaPagaPorPessoa = (valorDaGorgeta / numberDePessoas).toFixed(2);
+    const valorTotalApagar = ((valorConta + valorDaGorgeta) / numberDePessoas).toFixed(2);
+
+    return { gorgetaPagaPorPessoa, valorTotalApagar };
+}
+
 
 
 function calcularDesconto(valorConta, gorgeta, numberDePessoas) {
@@ -30,7 +56,6 @@ function calcularDesconto(valorConta, gorgeta, numberDePessoas) {
     const valorDaGorgeta = valorConta * gorgeta / 100;
     const gorgetaPagaPorPessoa = (valorDaGorgeta / numberDePessoas).toFixed(2);
     const valorTotalApagar = ((valorConta + valorDaGorgeta) / numberDePessoas).toFixed(2);
-    console.log({ numberDePessoas })
 
     return { gorgetaPagaPorPessoa, valorTotalApagar };
 
@@ -39,13 +64,27 @@ function calcularDesconto(valorConta, gorgeta, numberDePessoas) {
 function exibirConta(gorgetaPagaPorPessoa, valorTotalApagar, numberDePessoas) {
     const gorgeta = document.querySelector("#conteudo-TipAmount")
     const total = document.querySelector("#conteudo-total")
-    const msgErro = document.querySelector('.conteudo-error')
+    const msgErro = document.querySelector('.conteudo_error')
+
     gorgeta.textContent = `$${ gorgetaPagaPorPessoa}`;
     total.textContent = `$${valorTotalApagar}`;
 
-    if (numberDePessoas === '') {
+    if (numberDePessoas === 0) {
         msgErro.style.display = "flex";
+
     }
 
-
 }
+
+const rest = document.querySelector(".conteudo__reset")
+
+rest.addEventListener("click", (ev) => {
+
+    console.log("resetValores")
+    const resetPagaPorPessoa = document.querySelector("#conteudo-TipAmount")
+    const valorTotalApagar = document.querySelector("#conteudo-total")
+    resetPagaPorPessoa.textContent = `$0.00`;
+    valorTotalApagar.textContent = `$0.00`;
+
+
+})
